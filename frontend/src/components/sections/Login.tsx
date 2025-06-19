1abd12cbf677e1b65f6150e8ea675f94390635ce
Olivia Jardine <oliviafjardine@gmail.com>
2025-06-19 13:45:07 -0700

    Added error message to Login/signup

--- frontend/src/components/sections/Login.tsx
+++ frontend/src/components/sections/Login.tsx
@@ -6,17 +6,22 @@ import { useAuth } from '../../context/AuthContext'
 export default function Login() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
   const [isRegister, setIsRegister] = useState(false)
   const [authenticating, setAuthenitcating] = useState(false)
   const { signup, login } = useAuth()
 
   async function handleSubmit() {
-    if (!email || !password || password.length < 8){
+    if (!email || !password || password.length < 8) {
+      setError('Please enter a valid email and password')
       return
     }
+  
+    setError('')
     setAuthenitcating(true)
+  
     try {
-      if (isRegister){
+      if (isRegister) {
         console.log('Signing up a new user')
         await signup(email, password)
       } else {
@@ -25,13 +30,13 @@ export default function Login() {
       }
     } catch (err) {
       if (err instanceof Error) {
-        console.log(err.message)
+        setError(err.message)
       } else {
-        console.log('Unknown error during auth')
+        setError('Unknown error during authentication.')
       }
     } finally {
       setAuthenitcating(false)
-    }
+    }  
   }
 
   return (
@@ -52,9 +57,16 @@ export default function Login() {
       }} className='w-full max-w-[300px] mx-auto px-3 duration-200 hover:border-blue-600 focus:border-blue-600 py-2 sm:py-3 border border-solid border-blue-400 rounded-full outline-none'
         placeholder='Password' type='password' />
       
+      {/* Error message */}
+      {error && (
+        <div className="text-red-600 text-sm max-w-[300px] text-center">
+          {error}
+        </div>
+      )}
+      
       {/* Submit button */}
       <div className='max-w-[300px] w-full mx-auto'>
-        <Button onClick={handleSubmit} text={authenticating ? 'Submitting' : 'Submit'} full/>
+        <Button onClick={handleSubmit} text={authenticating ? 'Submitting' : 'Submit'} full dark />
       </div>
 
       {/* Sign up link */}
