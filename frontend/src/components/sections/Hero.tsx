f51046d4dc04844dff355469d427585ad2e8dbba
Olivia Jardine <oliviafjardine@gmail.com>
2025-06-19 02:26:32 -0700

    Added Go to Dashboard when logged in

--- frontend/src/components/sections/Hero.tsx
+++ frontend/src/components/sections/Hero.tsx
@@ -1,6 +1,5 @@
-import React from 'react'
-import Button from '../ui/Button'
-import Link from 'next/link'
+import React from 'react';
+import CallToAction from './CallToAction';
 
 export default function Hero() {
   return (
@@ -9,14 +8,7 @@ export default function Hero() {
         <span className='textGradient'>Swello </span>
         is a platform built to boost your career in tech — from community help to coding interviews in software, AI, and data fields.
       </h1>
-      <div className='grid grid-cols-2 gap-4 w-fit mx-auto'>
-        <Link href={'/dashboard'}>
-          <Button text="Sign Up" />
-        </Link>
-        <Link href={'/dashboard'}>
-          <Button text="Login" dark />
-        </Link>
-      </div>
+      <CallToAction />
     </div>
   )
 }
